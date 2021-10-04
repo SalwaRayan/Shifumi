678f53dae4046d3c35a17b38430036d6aff1fe8b
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-04 17:27:52 +0200

    shifumi

--- index.js
+++ index.js
@@ -15,21 +15,15 @@ var cideaux = document.getElementById("cideaux")
 
 function onButtonClickPierre(){
   imageNous.setAttribute("src", "https://www.jerome-reaux-creations.fr/DVP/codepen/shifumi/1-pierre.jpg")
-  for (var i = 0; i < img.length; i++){
-    imageIA = Math.floor(Math.random()*img.length)
-  }
+
 }
 function onButtonClickFeuille(){
   imageNous.setAttribute("src", "https://www.jerome-reaux-creations.fr/DVP/codepen/shifumi/2-feuille.jpg")
-  for (var i = 0; i < img.length; i++){
-    imageIA = Math.floor(Math.random()*img.length)
-  }
+
 }
 function onButtonClickCideaux(){
   imageNous.setAttribute("src", "https://jerome-reaux-creations.fr/DVP/codepen/shifumi/3-ciseaux.jpg")
-  for (var i = 0; i < img.length; i++){
-    imageIA = Math.floor(Math.random()*img.length)
-  }
+
 }
 
 
